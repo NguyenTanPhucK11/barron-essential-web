98ba593e6858d505bc79933bc843f0d2484bcb2e
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2021-07-27 23:45:55 +0700

    sign in, sign out

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import thunk from "redux-thunk";
-import { getFirestore } from "redux-firestore";
-import { getFirebase } from "react-redux-firebase";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import { reduxFirestore, getFirestore } from "redux-firestore";
+// import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+// import fbConfig from "./config/fbConfig";
+// import rootReducer from "./store/reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))
-);
+// const store = createStore(
+//   //   rootReducer,
+//   compose(
+//     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+//     reduxFirestore(),
+//     reactReduxFirebase()
+//   )
+// );
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,21 @@
 export const createProject = (project) => {
-  return (dispatch, getState, {getFirebase, getFirestore}) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("Vocabulary")
+      .add({
+        ...project,
+        authorFirstName: "a",
+        authorLastName: "b",
+        authorId: "c",
+        createAt: "d",
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch((err) => {
+        dispatch({ type: "CREATE_PROJECT_ERROR", err });
+      });
     dispatch({ type: "CREATE_PROJECT", project });
   };
 };
