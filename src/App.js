4863e810179e97071f63f625448c6cb48b42a32a
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2021-07-28 18:26:25 +0700

    reset shuffle

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Dashboard from "./components/dashboard/Dashboard";
-import ProjectDetails from "./components/project/ProjectDetails";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import "./components/project/CreateProject.js";
+
 import Page from "./components/Page";
-import Timer from "./components/Timer";
-import { createProject } from "./store/actions/projectActions";
-import CreateProject from "./components/project/CreateProject.js";
-import SignIn from "./components/auth/SignIn";
-import SignUp from "./components/auth/SignUp";
 function App() {
   return (
     <div className="body-style">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/project/:id" component={ProjectDetails} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/signup" component={SignUp} />{" "}
-          <Route path="/create" component={CreateProject} />
-        </Switch>
-      </BrowserRouter>
-      {/* <CreateProject /> */}
+      <Page />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,26 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { reduxFirestore, getFirestore } from "redux-firestore";
-import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import fbConfig from "./config/fbConfig";
-import { Provider } from "react-redux";
-import rootReducer from "./store/reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reduxFirestore(fbConfig),
-    // reactReduxFirebase(fbConfig) 
-  )
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
