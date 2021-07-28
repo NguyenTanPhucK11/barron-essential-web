19898b75c4404cff2af664d45b9905ae36ad8c8d
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2021-07-28 09:57:00 +0700

    use redux

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { createStore, applyMiddleware, compose } from "redux";
+import { createStore } from "redux";
 // import thunk from "redux-thunk";
 // import { reduxFirestore, getFirestore } from "redux-firestore";
 // import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 // import fbConfig from "./config/fbConfig";
 // import rootReducer from "./store/reducers/rootReducer";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
 
-// const store = createStore(
-//   //   rootReducer,
-//   compose(
-//     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-//     reduxFirestore(),
-//     reactReduxFirebase()
-//   )
-// );
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,22 +1,23 @@
-// const initState = {
-//   project: [
-//     { id: "1", title: "11", content: "111" },
-//     { id: "2", title: "22", content: "222" },
-//     { id: "3", title: "33", content: "333" },
-//   ],
-// };
+const initState = {
+  projects: [
+    { id: "1", title: "11", content: "111" },
+    { id: "2", title: "22", content: "222" },
+    { id: "3", title: "33", content: "333" },
+  ],
+};
 
-// const projectReducer = (state = initState, action) => {
-//   switch (actionTypes.type) {
-//     case "CREATE_PROJECT":
-//       console.log("created project", action.project);
-//       return state;
-//     case "CREATE_PROJECT_ERROR":
-//       console.log("create project error", action.err);
-//       return state;
-//     default:
-//       return state;
-//   }
-// };
+const projectReducer = (state = initState, action) => {
+  //   switch (actionTypes.type) {
+  //     case "CREATE_PROJECT":
+  //       console.log("created project", action.project);
+  //       return state;
+  //     case "CREATE_PROJECT_ERROR":
+  //       console.log("create project error", action.err);
+  //       return state;
+  //     default:
+  //       return state;
+  //   }
+  return state;
+};
 
-// export default projectReducer;
+export default projectReducer;
